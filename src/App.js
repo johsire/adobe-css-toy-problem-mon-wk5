245561007e30bd803239d91dc35b921642494d52
Josiah Maina <josiah5115@gmail.com>
2018-07-23 09:25:44 -0600

    our divs

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ class App extends Component {
           width: "100%",
           height: "100vh",
           backgroundColor: "grey",
-          display: "flex"
+          display: "flex",
+          flexDirection: "column"
         }}
       >
-        Our Canvas
+      <div style={{backgroundColor: "green", height: "60px" }}>Header</div>
+      <div style={{backgroundColor: "yellow", height: "60px"}}>SubHeader</div>
+      <div style={{backgroundColor: "green", height: "240px"}}>Content</div>
+      <div style={{backgroundColor: "purple", height: "240px"}}>Image</div>
+
       </div>
     );
   }
