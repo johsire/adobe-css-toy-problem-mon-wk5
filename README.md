7a3b46d5119b92b6187802340d37a1120327612c
Josiah Maina <josiah5115@gmail.com>
2018-07-23 09:19:38 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![](/public/adobe.PNG)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div
+        className="App"
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "grey",
+          display: "flex"
+        }}
+      >
+        Our Canvas
+      </div>
+    );
+  }
+}
+
+export default App;
